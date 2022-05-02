fac73d0a17311b205c71aaaeb37e341d920f90f1
neo0112358 <borjaruizreverter@gmail.com>
2022-05-02 16:28:48 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      
+      <div class="parallax-img-container">
+        <h1>The day Internet was the future</h1>
+        <p>Some static text that will not move</p>
+        <img class="parallax-move" src="./BG.svg" />
+        <div class="parallax-move">something here</div>
+        <p class="parallax-move">text that will move</p>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
