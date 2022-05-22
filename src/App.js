2a91d9194f30a4505e95eaba63288f172a5fd0c0
neo0112358 <borjaruizreverter@gmail.com>
2022-05-22 14:34:48 -0300

    Section1

--- src/App.js
+++ src/App.js
@@ -1,80 +1,41 @@
 import React from "react";
-import BG from "./images/BG.svg";
-import Titulo from "./title.js";
 import SubTitulo from "./subtitle.js";
 import "./App.css";
 import PrimaryButtons from "./images/PrimaryButtons.png";
+import { TitleSvg } from "./images/titlesvg.jsx";
+import Codebox1 from "./codebox1";
 
 function App() {
   return (
     <>
       <div className="stars">
         <div className="twinkling">
-          <Titulo />
-          <SubTitulo />
-          <div className="codebox">
-            <pre>
-              <code className="language-javascript">
-                <span className="import-color">import </span>
-                <span>&#65115;</span>
-                <span className="imported-components-color">Button</span>
-                <span>,</span>
-                <span className="imported-components-color">Card</span>
-                <span>&#65116;</span>
-                <span className="import-color">from </span>
-                <span>'Cyberfuture'</span>
-                <br></br><br></br>
-                <span >&lt;</span>
-                <span className="components-color">Button </span>
-                <span className="props-color">text</span>
-                <span>='Button' </span>
-                <span className="props-color">variant</span>
-                <span>='Matrix' </span>
-                <span className="props-color">color</span>
-                <span>='Gold'</span>
-                <span >/</span>
-                <span >&gt;</span>
-                <br></br>
-                <span >&lt;</span>
-                <span className="components-color">Button </span>
-                <span className="props-color">text</span>
-                <span>='Button' </span>
-                <span className="props-color">variant</span>
-                <span>='OutlinedCyberRose' </span>
-                <span className="props-color">color</span>
-                <span>='Matrix'</span>
-                <span >/</span>
-                <span >&gt;</span>
-                <br></br>
-                <span >&lt;</span>
-                <span className="components-color">Card </span>
-                <span className="props-color">variant</span>
-                <span>='Matrix' </span>
-                <span className="props-color">title</span>
-                <span>='Card Title' </span>
-                <span className="props-color">variant</span>
-                <span>='Matrix'</span>
-                <span >/</span>
-                <span >&gt;</span>
-              </code>
-            </pre>
+          <section id="FirstSection">
+            <div className="title">
+              <TitleSvg />
+            </div>
+            <div className="below-title">
+              <SubTitulo />
+              <Codebox1 />
+            </div>
+          </section>
+
+          <div className="paragraph">
+            <h2>
+              You can use CyberFuture`s custom styles for give some action to
+              your boring components.
+            </h2>
           </div>
           <div className="paragraph">
-          <h2>You can use CyberFuture`s custom styles for give some action to
-              your boring components.</h2>
+            <p></p>
           </div>
           <div className="paragraph">
-            <p>
-            </p>
+            <h1>Buttons</h1>
+            <h2>Primary buttons</h2>
           </div>
+          <div className="paragraph"></div>
           <div className="paragraph">
-          <h1>Buttons</h1>
-              <h2>Primary buttons</h2>
-          </div>
-          <div className="paragraph">
-          </div>
-          <div className="paragraph">
-            <img src={PrimaryButtons} alt="PrimaryButtons" width='100%'/>
+            <img src={PrimaryButtons} alt="PrimaryButtons" width="100%" />
           </div>
           <div className="codebox2">
             <pre>
@@ -87,51 +48,52 @@ function App() {
                 <span>&#65116;</span>
                 <span className="import-color">from </span>
                 <span>'Cyberfuture'</span>
-                <br></br><br></br>
-                <span >&lt;</span>
+                <br></br>
+                <br></br>
+                <span>&lt;</span>
                 <span className="components-color">Button </span>
                 <span className="props-color">text</span>
                 <span>='ENABLED' </span>
                 <span className="props-color">variant</span>
                 <span>='Punk' </span>
-                <span >/</span>
-                <span >&gt;</span>
+                <span>/</span>
+                <span>&gt;</span>
                 <br></br>
-                <span >&lt;</span>
+                <span>&lt;</span>
                 <span className="components-color">Button </span>
                 <span className="props-color">text</span>
                 <span>='ENABLED' </span>
                 <span className="props-color">variant</span>
                 <span>='Rose' </span>
-                <span >/</span>
-                <span >&gt;</span>
+                <span>/</span>
+                <span>&gt;</span>
                 <br></br>
-                <span >&lt;</span>
+                <span>&lt;</span>
                 <span className="components-color">Button </span>
                 <span className="props-color">text</span>
                 <span>='ENABLED' </span>
                 <span className="props-color">variant</span>
                 <span>='OutlinedCyberSly' </span>
-                <span >/</span>
-                <span >&gt;</span>
+                <span>/</span>
+                <span>&gt;</span>
                 <br></br>
-                <span >&lt;</span>
+                <span>&lt;</span>
                 <span className="components-color">Button </span>
                 <span className="props-color">text</span>
                 <span>='ENABLED' </span>
                 <span className="props-color">variant</span>
                 <span>='Gold' </span>
-                <span >/</span>
-                <span >&gt;</span>
+                <span>/</span>
+                <span>&gt;</span>
                 <br></br>
-                <span >&lt;</span>
+                <span>&lt;</span>
                 <span className="components-color">Button </span>
                 <span className="props-color">text</span>
                 <span>='ENABLED' </span>
                 <span className="props-color">variant</span>
                 <span>='Matrix' </span>
-                <span >/</span>
-                <span >&gt;</span>
+                <span>/</span>
+                <span>&gt;</span>
                 <br></br>
               </code>
             </pre>
@@ -140,9 +102,9 @@ function App() {
           <h1>Cards</h1>
               <h2>You can use CyberFuture`s custom cards that provide a flexible and extensible content for containers with multiple variants and options.</h2>
           </div> */}
-          <div className="Ground">
+          {/* <div className="Ground">
             <img src={BG} alt="Ground" />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
---
+++ src/codebox1.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Codebox1 = () => {
+    return (
+        <div className="codebox">
+                <pre>
+                  <code className="language-javascript">
+                    <span className="import-color">import </span>
+                    <span>&#65115;</span>
+                    <span className="imported-components-color">Button</span>
+                    <span>,</span>
+                    <span className="imported-components-color">Card</span>
+                    <span>&#65116;</span>
+                    <span className="import-color">from </span>
+                    <span>'Cyberfuture'</span>
+                    <br></br>
+                    <br></br>
+                    <span>&lt;</span>
+                    <span className="components-color">Button </span>
+                    <span className="props-color">text</span>
+                    <span>='Button' </span>
+                    <span className="props-color">variant</span>
+                    <span>='Matrix' </span>
+                    <span className="props-color">color</span>
+                    <span>='Gold'</span>
+                    <span>/</span>
+                    <span>&gt;</span>
+                    <br></br>
+                    <span>&lt;</span>
+                    <span className="components-color">Button </span>
+                    <span className="props-color">text</span>
+                    <span>='Button' </span>
+                    <span className="props-color">variant</span>
+                    <span>='OutlinedCyberRose' </span>
+                    <span className="props-color">color</span>
+                    <span>='Matrix'</span>
+                    <span>/</span>
+                    <span>&gt;</span>
+                    <br></br>
+                    <span>&lt;</span>
+                    <span className="components-color">Card </span>
+                    <span className="props-color">variant</span>
+                    <span>='Matrix' </span>
+                    <span className="props-color">title</span>
+                    <span>='Card Title' </span>
+                    <span className="props-color">variant</span>
+                    <span>='Matrix'</span>
+                    <span>/</span>
+                    <span>&gt;</span>
+                  </code>
+                </pre>
+              </div>
+    );
+}
+
+export default Codebox1;
